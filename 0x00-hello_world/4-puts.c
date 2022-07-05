b1df23fe54f9a197f73348b78ce2a6c6d6367fda
Joseph3545 <tobbyjoseph@hotmail.co.za>
2022-07-05 03:01:18 -0700

    nineth commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
-/**
+
+*/*
  *
- * * main - Prints using puts() command
+ * main - Entry point
  *
- * * Return: 0 prints something
+ * Return: Always 0 (Success)
  *
- * */
+ */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
+
 }
-
-
