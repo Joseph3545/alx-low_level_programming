472c9bf2843d2e264a99205079606122ad313efe
Joseph3545 <tobbyjoseph@hotmail.co.za>
2022-07-07 04:27:14 -0700

    last commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 	{
 	_putchar('0');
 	_putchar(',');
-	_putchar( ' ' );
+	_putchar(' ');
 	for (cone = 1; cone <= 9; cone++)
 	{
 		d = (rone * cone);
@@ -24,12 +24,12 @@ void times_table(void)
 		}
 		else
 		{		
-			_putchar(' ');
+				_putchar(' ');
 		}
 		_putchar((d % 10) + '0');
 			if (cone < 9)
 			{
-				_putchar( ',' );
+				_putchar(',');
 				_putchar(' ');
 			}
 	}
