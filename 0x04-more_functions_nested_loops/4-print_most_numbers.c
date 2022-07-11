df2cea9a2129db2c88604a9f846b645032b43bb7
Joseph3545 <tobbyjoseph@hotmail.co.za>
2022-07-11 02:34:23 -0700

    hot fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  *print_most_numbers -> this prints most numbers
- *
  */
 void print_most_numbers(void)
 {
@@ -13,5 +12,5 @@ void print_most_numbers(void)
 		if (x != 2 && x != 4)
 		_putchar(x + '0');
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
