497433f302a22e7f9c68c0872f5479f42d0eeb7e
Joseph3545 <tobbyjoseph@hotmail.co.za>
2022-07-11 16:00:23 -0700

    fixed code

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * _strlen - find the length of a string
  * @s: pointer to the string to check
@@ -8,7 +10,6 @@
 int _strlen(char *s)
 {
 	int i = 0;
-
 	while (s[i])
 		i++;
 
